01f2b023ce92cee374e9eda9f77d89fc10b0fc3b
abdurrahmankucuk90 <abdurrahmankucuk90@gmail.com>
2022-09-11 11:06:58 +0200

    finished

--- todo/form.py
+++ todo/form.py
@@ -1,9 +1,13 @@
+import re
 from django import forms
 from django.forms import fields
 from .models import Todo
 
 
-class Todo(forms.ModelForm):
+
+class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = "__all__"
+
+
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from .models import Todo
+from .form import TodoForm
 
 
 def home(request):
@@ -9,3 +10,18 @@ def home(request):
         'todos' : todos
     }
     return render(request, 'todo/home.html', context)
+
+# class todo_create(request):
+#     form = TodoForm()
+#     context = {
+#         'form':form
+#     }
+#     return render(request, 'todo/todo_add.html', context)
+
+def todo_create(request):
+    form = TodoForm()
+    context ={
+        'form' : form
+    }
+    return render(request, 'todo/todo_add.html', context)
+
